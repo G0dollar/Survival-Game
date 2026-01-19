ecc8541d0c6a18aaee69af91cf79423c3c5ff5fc
G0dollar <goddollar69@gmail.com>
2026-01-19 20:02:31 +0530

    Update Three.js

--- Three.js
+++ Three.js
@@ -1,8 +1,8 @@
-new Three.Scene();
+new THREE.Scene();
 const aspect = window.innerWidth / window.innerHeight;
 let FOV = 90;
 new THREE.PerspectiveCamera(FOV, aspect , 0.1 , 1000);
-new Three.WebGLRenderer();
+new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 scene.background = new THREE.Color(0x87ceeb);
