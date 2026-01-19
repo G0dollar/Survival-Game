500e2b6368fa2408f375c3251fee4f87e218e9a1
G0dollar <goddollar69@gmail.com>
2026-01-19 20:07:59 +0530

    Update Three.js

--- Three.js
+++ Three.js
@@ -1,12 +1,15 @@
-new THREE.Scene();
 const scene = new THREE.Scene();
+
 const aspect = window.innerWidth / window.innerHeight;
 let FOV = 90;
-new THREE.PerspectiveCamera(FOV, aspect , 0.1 , 1000);
 const camera = new THREE.PerspectiveCamera(FOV, aspect, 0.1, 1000);
-new THREE.WebGLRenderer();
+
+camera.position.z = 5; // so camera is not at the origin
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
 scene.background = new THREE.Color(0x87ceeb);
-renderer.render(scene, camera);
+
+renderer.render(scene, camera); // renders once
